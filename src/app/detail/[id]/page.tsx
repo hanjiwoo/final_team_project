b777586cadbe64dcf9f6b3fbc96a22200d890aee
hanjiwoo <hanmingyu3@naver.com>
2024-01-05 21:58:50 +0900

    first push

---
+++ src/app/detail/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DetailPage() {
+  return <div className="bg-blue">디테일페이지야</div>;
+}
---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Mypage() {
+  return <div className="bg-blue">마이페이지야</div>;
+}
