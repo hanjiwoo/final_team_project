faead3ec2397737c1e07cc4e037880dfad6cddc9
BLACKPA1NT <yeo233@gmail.com>
2024-01-11 00:09:15 +0900

    feat: 회원가입 & 로그인 1차 UI 작업 완료 (#8)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,9 +7,11 @@ import {
 	GoogleAuthProvider,
 	GithubAuthProvider,
 	signInWithPopup,
+	OAuthProvider,
 } from "firebase/auth";
 // import { useNavigate } from "react-router-dom";
 import { auth, db, storage } from "../../shared/firebase";
+import axios from "axios";
 
 export default function Login() {
 	const [email, setEmail] = useState<string>("");
@@ -27,6 +29,34 @@ export default function Login() {
 			setDisplayName(user.displayName || null);
 		}
 	}, [user]);
+
+	// 카카오 로그인
+	useEffect(() => {
+		const code = new URL(window.location.href).searchParams.get("code");
+		getToken(code);
+	}, []);
+
+	const getToken = async (code: string | null) => {
+		try {
+			const response = await axios({
+				url: `https://kauth.kakao.com/oauth/token`,
+				method: "post",
+				params: {
+					grant_type: "authorization_code",
+					client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API,
+					redirect_uri: "http://localhost:3000/login", // 수정된 부분
+					code: code,
+				},
+			});
+
+			const access_token = response.data.access_token;
+			// 토큰을 사용할 수 있습니다. 원하는 작업을 수행하세요.
+			console.log(access_token);
+		} catch (error) {
+			// 오류 처리
+			console.error("Kakao 토큰 얻기 오류:", error);
+		}
+	};
 
 	const handleClickOnChange = (event: ChangeEvent<HTMLInputElement>) => {
 		const {
@@ -98,6 +128,7 @@ export default function Login() {
 				const credential: any = GoogleAuthProvider.credentialFromResult(res);
 				const token = credential.accessToken;
 				const userName = res.user.displayName;
+				const email = res.user.email;
 
 				// local storage에 token, username 저장해주기
 				console.log(token);
@@ -115,15 +146,16 @@ export default function Login() {
 		await signInWithGoogle();
 	};
 
-	// 깃허브 로그인
-	const signInWithGithub = async () => {
-		const githubProvider = new GithubAuthProvider();
+	//카카오 로그인
+	const signInWithKakao = () => {
+		const kakaoProvider = new OAuthProvider("https://kauth.kakao.com");
 
-		signInWithPopup(auth, githubProvider)
+		signInWithPopup(auth, kakaoProvider)
 			.then((res) => {
-				const credential: any = GithubAuthProvider.credentialFromResult(res);
+				const credential: any = OAuthProvider.credentialFromResult(res);
 				const token = credential.accessToken;
 				const userName = res.user.displayName;
+				const email = res.user.email;
 
 				// local storage에 token, username 저장해주기
 				console.log(token);
@@ -137,18 +169,30 @@ export default function Login() {
 	};
 
 	const handleClickKakao = async (event: FormEvent) => {
-		// event.preventDefault();
-		// await signInWithGithub();
+		event.preventDefault();
+		signInWithKakao();
 	};
 
 	return (
 		<div className="flex justify-center items-center w-screen h-screen">
-			<div className="bg-red-400">
-				<h2 className="flex justify-center">로그인</h2>
-				<form className="flex justify-center flex-col items-center">
-					<div>
-						<label>이메일</label>
+			<div className="w-[360px]">
+				<div className="mb-[52px]">
+					<h1 className="flex justify-left text-[32px] font-bold leading-42px w-full text-left">
+						로그인
+					</h1>
+					<span>
+						따뜻한 마음을 모아 당신에게 드려요 :)
+						<br />
+						모두의 음식점, 모음
+					</span>
+				</div>
+				<form className="flex justify-center flex-col items-center mb-[40px] gap-[16px]">
+					<div className="flex flex-col w-full">
+						<label className="test-[14px] leading-20px text-[#999]">
+							이메일
+						</label>
 						<input
+							className="h-[48px] px-[16px] rounded-[8px] border-solid border border-gray-400"
 							type="email"
 							value={email}
 							name="email"
@@ -158,10 +202,17 @@ export default function Login() {
 							onBlur={handleBlur}
 						></input>
 					</div>
-					{emailError && <div>{emailError}</div>}
-					<div>
-						<label>비밀번호</label>
+					{emailError && (
+						<div className="text-[#FF8145] mt-[-18px] mb-[-19px] text-[14px] w-full">
+							{emailError}
+						</div>
+					)}
+					<div className="flex flex-col w-full">
+						<label className="test-[14px] leading-20px text-[#999]">
+							비밀번호
+						</label>
 						<input
+							className="h-[48px] px-[16px] rounded-[8px] border-solid border border-gray-400"
 							type="password"
 							value={password}
 							name="password"
@@ -171,24 +222,42 @@ export default function Login() {
 							onBlur={handleBlur}
 						></input>
 					</div>
-					{passwordError && <div>{passwordError}</div>}
-					<button onClick={handleClickSignIn}>로그인</button>
-					<div>
-						<label>소셜계정으로 로그인</label>
-						<div className="flex">
-							<div className="cursor-pointer" onClick={handleClickGoogle}>
-								구글 들어갈 자리
-							</div>
-							<div className="cursor-pointer" onClick={handleClickKakao}>
-								카카오 들어갈 자리
-							</div>
-						</div>
-					</div>
-
-					<Link href="/join" className="cursor-pointer">
-						회원가입으로 이동
-					</Link>
 				</form>
+				{passwordError && (
+					<div className="text-[#FF8145] mt-[-18px] mb-[-19px] text-[14px] w-full">
+						{passwordError}
+					</div>
+				)}
+				<button
+					onClick={handleClickSignIn}
+					className="text-green-500 flex justify-center w-full rounded-[8px] h-[48px] items-center text-[#fff] bg-[#FF8145]"
+				>
+					로그인
+				</button>
+				<div className="flex flex-col">
+					<label>소셜계정으로 로그인</label>
+					<div className="flex flex-col gap-[12px]">
+						<div
+							className="cursor-pointer text-green-500 flex justify-center w-full rounded-[8px] h-[48px] items-center text-[#212121] bg-[#FFF]"
+							onClick={handleClickGoogle}
+						>
+							구글 들어갈 자리
+						</div>
+						{/* <div className="cursor-pointer" onClick={handleClickKakao}>
+								카카오 들어갈 자리
+							</div> */}
+						<Link
+							href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API}&redirect_uri=http://localhost:3000/login`}
+							className="text-green-500 flex justify-center w-full rounded-[8px] h-[48px] items-center text-[#212121] bg-[#FEE500]"
+						>
+							카카오로 로그인
+						</Link>
+					</div>
+				</div>
+
+				<Link href="/join" className="cursor-pointer">
+					회원가입으로 이동
+				</Link>
 			</div>
 		</div>
 	);
