7ff74f677f81e6d67194479b1f2cac9674fc982d
아름 <aaarem17@gmail.com>
2024-01-11 15:15:01 +0900

    feat: 이달의 best 매장 반응형으로 구현중 (#16)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import Best from "@/components/home/Best";
 import MainBanner from "@/components/home/MainBanner";
 import NowLocationBtn from "@/components/home/NowLocationBtn";
 import SearchForm from "@/components/home/SearchForm";
-import ShopList from "@/components/home/ShopList";
 import ShopList_map from "@/components/home/ShopList_map";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-[95vh] bg-blue-200 gap-10">
+    <div className="flex flex-col items-center h-[100%] bg-blue-200 gap-10">
       <MainBanner />
       <SearchForm />
 
