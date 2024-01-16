5c42e8e7dded8e2c07596fa34408314edb0c4334
BLACKPA1NT <yeo233@gmail.com>
2024-01-17 00:09:25 +0900

    feat: 메인페이지 1차 디자인 (#36)

--- src/components/home/Best.tsx
+++ src/components/home/Best.tsx
@@ -1,65 +1,73 @@
 import React from "react";
-import dummy from "../../app/assets/images/피카츄.jpg";
+import dummy from "../../app/assets/images/fakeLogo.jpg";
 import Image from "next/image";
+import heart from "../../app/assets/images/icon/heart_off.png";
 
 const photo1 = dummy;
 const photo2 = dummy;
 const photo3 = dummy;
 export const bestList = [
-  {
-    id: "1",
-    title: "매장 이름",
-    photo: photo1,
-    추천해요: "여기는 따봉갯수",
-  },
-  {
-    id: "2",
-    title: "매장 이름",
-    photo: photo2,
-    추천해요: "여기는 따봉갯수",
-  },
-  {
-    id: "3",
-    title: "매장 이름",
-    photo: photo3,
-    추천해요: "여기는 따봉갯수",
-  },
+	{
+		id: "1",
+		title: "매장 이름",
+		photo: photo1,
+		추천해요: "여기는 따봉갯수",
+	},
+	{
+		id: "2",
+		title: "매장 이름",
+		photo: photo2,
+		추천해요: "여기는 따봉갯수",
+	},
+	{
+		id: "3",
+		title: "매장 이름",
+		photo: photo3,
+		추천해요: "여기는 따봉갯수",
+	},
 ];
 
 export default function Best() {
-  return (
-    <div className="container px-5 py-10 mx-auto">
-      <div className="text-center mb-12">
-        <h1 className="text-4xl md:text-4xl text-gray-700 font-semibold">
-          이달의 Best 매장 모-음
-        </h1>
-      </div>
+	return (
+		<div className="container px-5 py-40 mx-auto">
+			<div className="text-center mb-12">
+				<h1 className="text-[28px] leading-[36px] text-[#212121] font-semibold mb-[12px]">
+					이달의 Best 매장 모-음
+				</h1>
+				<span className="text-[18px] leading-[20px] font-[#5c5c5c]">
+					인기 매장을 지금 바로 확인해보세요 :)
+				</span>
+			</div>
 
-      <div className="flex flex-wrap -m-4">
-        {bestList.map((item) => {
-          return (
-            <div className="p-4 sm:w-1/3 lg:w-1/3" key={item.id}>
-              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <Image
-                  className="lg:h-72 md:h-48 w-full object-cover object-center"
-                  src={item.photo}
-                  alt="photo"
-                />
+			<div className="flex justify-center items-center">
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 gap-[24px]">
+					{bestList.map((item) => {
+						return (
+							<div key={item.id}>
+								<div className="h-full rounded-lg overflow-hidden hover:scale-105 transition ease-in transition-property: all transition-duration: 0.5s w-[344px] h-[324px]">
+									<Image
+										className="w-[344px] h-[324px] object-cover object-center rounded-[12px] mb-[20px]"
+										src={item.photo}
+										alt="photo"
+									/>
 
-                <div
-                  className="p-6 hover:bg-orange-400 hover:text-white
-                         transition duration-300 ease-in"
-                >
-                  {item.title}
-                </div>
-                <div className="text-base font-medium text-indigo-400 mb-1">
-                  {item.추천해요}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+									<span className=" text-[16px] text-[#212121] leading-[24px] font-semibold">
+										{item.title}
+									</span>
+									<div className="mb-1 flex items-center gap-[4px] text-[#FF8145] text-[14px] leading-[20px]">
+										<Image
+											src={heart}
+											alt="like"
+											className="w-[20px] h-[20px]"
+										/>
+										{item.추천해요}
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/home/ShopList.tsx
+++ src/components/home/ShopList.tsx
@@ -15,90 +15,94 @@ import Image from "next/image";
 import place from "../../app/assets/images/icon/place.png";
 
 export default function ShopList() {
-  const shops = useSelector((state: RootState) => state.shops);
-  const dispatch = useDispatch();
-  // console.log(shops, " 일단 레절트");
-  const [slide, setSlide] = useState<number>(0);
-  const router = useRouter();
+	const shops = useSelector((state: RootState) => state.shops);
+	const dispatch = useDispatch();
+	// console.log(shops, " 일단 레절트");
+	const [slide, setSlide] = useState<number>(0);
+	const router = useRouter();
 
-  // console.log(Math.ceil(shops.length / 4), "길이 알아보자");
+	// console.log(Math.ceil(shops.length / 4), "길이 알아보자");
 
-  const rigthMove = () => {
-    if (Math.ceil((shops.length / 4) * 1000) + slide - 1500 <= 0) return;
-    // console.log(Math.ceil(shops.length / 4) * 1000, "흐음", slide);
-    setSlide(slide - 500);
-  };
-  const leftMove = () => {
-    if (slide >= 0) return;
-    setSlide(slide + 500);
-  };
+	const rigthMove = () => {
+		if (Math.ceil((shops.length / 4) * 1000) + slide - 1500 <= 0) return;
+		// console.log(Math.ceil(shops.length / 4) * 1000, "흐음", slide);
+		setSlide(slide - 500);
+	};
+	const leftMove = () => {
+		if (slide >= 0) return;
+		setSlide(slide + 500);
+	};
 
-  const moveToFullMap = () => {
-    router.push("/map");
-  };
-  return (
-    <>
-      <div className="container  py-10  bg-red-400 w-full relative">
-        <div className="text-center mb-12 bg-blue-200">
-          <h1 className="text-4xl md:text-4xl text-gray-700 font-semibold">
-            내 주변의 모-음은 어디일까요?
-          </h1>
-          <div>
-            {shops[0]?.시도 === "" ? (
-              <div> 검색이 필요해요</div>
-            ) : (
-              <div className="flex justify-center gap-2">
-                현재 <Image src={place} alt="위치마크" />
-                <p className="font-bold">
-                  {shops[0]?.시도} {shops[0]?.시군}
-                </p>
-                기준이에요
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="overflow-hidden">
-          <div
-            style={{
-              backgroundColor: "green",
-              width: `${Math.ceil(shops.length / 4) * 1000}px`,
-              transform: `translate(${slide}px)`,
-              transition: "transform 0.5s",
-              display: "flex",
-            }}
-          >
-            {shops.map((shop: typeOfShop) => {
-              return (
-                <React.Fragment key={nanoid()}>
-                  <ShopCard shop={shop} shops={shops} />
-                </React.Fragment>
-              );
-            })}
-          </div>
-        </div>
-        <section className="flex bg-blue-300 justify-around">
-          <button
-            className="bg-purple-300 rounded-full text-4xl hover:scale-110 absolute top-[200px] left-[-60px]"
-            onClick={leftMove}
-          >
-            👈
-          </button>
-          <button
-            className="bg-purple-300 rounded-full text-4xl hover:scale-110 absolute top-[200px] right-[-60px]"
-            onClick={rigthMove}
-          >
-            👉
-          </button>
-          {shops[0] && (
-            <button
-              onClick={moveToFullMap}
-              className="bg-orange-500 w-[50px] h-[50px] rounded-full absolute top-[45px] right-[10px] hover:scale-105"
-            >
-              맵보러가기
-            </button>
-          )}
-        </section>
-      </div>
-    </>
-  );
+	const moveToFullMap = () => {
+		router.push("/map");
+	};
+	return (
+		<>
+			<div className="container  py-[40px] w-full relative">
+				<div className="text-center mb-12">
+					<h1 className="text-[28px] text-[#212121] font-semibold leading-[36px] mb-[12px]">
+						내 주변의 모-음은 어디일까요?
+					</h1>
+					<div className="mb-[60px]">
+						{shops[0]?.시도 === "" ? (
+							<div> 검색이 필요해요</div>
+						) : (
+							<div className="flex justify-center gap-2 font-[18px] font-[500] leading-[26px]">
+								현재{" "}
+								<Image
+									src={place}
+									alt="위치마크"
+									className="w-[24px] h-[24px]"
+								/>
+								<p className="font-bold text-[18px] leading-[26px]">
+									{shops[0]?.시도} {shops[0]?.시군}
+								</p>
+								기준이에요
+							</div>
+						)}
+					</div>
+				</div>
+				<div className="flex justify-center items-center">
+					<div
+						style={{
+							transform: `translate(${slide}px)`,
+							transition: "transform 0.5s",
+							display: "flex",
+						}}
+						className="overflow-hidden flex gap-[24px] justify-center items-center"
+					>
+						{shops.map((shop: typeOfShop) => {
+							return (
+								<React.Fragment key={nanoid()}>
+									<ShopCard shop={shop} shops={shops} />
+								</React.Fragment>
+							);
+						})}
+					</div>
+				</div>
+				<section className="flex bg-blue-300 justify-around">
+					<button
+						className="bg-purple-300 rounded-full text-4xl hover:scale-110 absolute top-[200px] left-[-60px]"
+						onClick={leftMove}
+					>
+						👈
+					</button>
+					<button
+						className="bg-purple-300 rounded-full text-4xl hover:scale-110 absolute top-[200px] right-[-60px]"
+						onClick={rigthMove}
+					>
+						👉
+					</button>
+					{shops[0] && (
+						<button
+							onClick={moveToFullMap}
+							className="bg-orange-500 w-[50px] h-[50px] rounded-full absolute top-[45px] right-[10px] hover:scale-105"
+						>
+							맵보러가기
+						</button>
+					)}
+				</section>
+			</div>
+		</>
+	);
 }
