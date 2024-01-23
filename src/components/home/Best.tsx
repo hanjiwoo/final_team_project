5b15a51c287365deb3830a8559896ebf1c870f38
BLACKPA1NT <yeo233@gmail.com>
2024-01-23 23:57:57 +0900

    feat: 중간커밋 (#62)

--- src/components/home/Best.tsx
+++ src/components/home/Best.tsx
@@ -79,7 +79,7 @@ export default function Best() {
 			</div>
 
 			<div className="flex justify-center items-center">
-				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-[24px]">
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-[24px]">
 					{/* {bestList.map((item) => {
 						return (
 							<div key={item.id}>
