ee58e46e735cf3a9c2a0f860608e0cd2de9f10a9
hanjiwoo <hanmingyu3@naver.com>
2024-01-24 16:46:46 +0900

    feat:마이페이지 내 게시글 내 가게 보기 샵카드에 후기 추가(#39)

--- src/app/mypage/edit/page.tsx
+++ src/app/mypage/edit/page.tsx
@@ -84,10 +84,7 @@ export default function Page() {
         <div className="flex flex-col justify-center items-center w-[350px] h-[545px]">
           <div>
             <label>
-              <div className="w-[100px] h-[100px] overflow-hidden rounded-full">
-                {/* <Image
-
-              </div>
+              <div className="w-[100px] h-[100px] overflow-hidden rounded-full"></div>
               <Image
                 src={cameraImage}
                 alt="profileImageEditButton"
